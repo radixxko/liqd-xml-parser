1818d0e3ec95d53938e581b963e831b238aa6f28
Tomas Korenko <radixxko@gmail.com>
2019-10-26 12:16:30 +0200

    Version 1.2.1

--- test/test.js
+++ test/test.js
@@ -1,4 +1,16 @@
 const fs = require('fs');
 const XML = require('../lib/xml_parser');
 
-const xml = new XML( fs.readFileSync( __dirname + '/HDT-feed.xml', 'utf8' ));
+const feed = fs.readFileSync( __dirname + '/HDT-feed.xml', 'utf8' );
+
+for( let i = 0; i < 5; ++i )
+{
+    
+    let start = process.hrtime();
+
+    const xml = new XML( feed );
+
+    let end = process.hrtime(start);
+
+    console.log( xml, ( end[0] * 1000 + end[1] / 1e6 ) + 'ms' );
+}
